3135600ab04b51568831888f471c992bc7fe26e6
Daniel Choi <daniel.choi@mvp.studio>
2019-12-13 12:23:19 +1100

    init

---
+++ MarsQA-1/SpecflowPages/Pages/SignIn.cs
@@ -0,0 +1,28 @@
+﻿using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace MarsQA_1.Pages
+{
+    public class SignIn
+    {
+        
+        public static void SigninStep()
+        {
+            Driver.NavigateUrl();
+
+            //Enter Url
+            Driver.driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']")).Click();
+
+            //Enter Username
+            Driver.driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]")).SendKeys("");
+
+            //Enter password
+            Driver.driver.FindElement(By.XPath("//INPUT[@type='password']")).SendKeys("");
+
+            //Click on Login Button
+            Driver.driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']")).Click();
+
+        }
+    }
+}
