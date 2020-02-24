4321168a2d692bf2b6d688f976acb1125984ed62
Daniel.C <choiwaim@gmail.com>
2020-02-24 11:02:56 +1100

    update framework

--- MarsQA-1/SpecflowPages/Pages/SignIn.cs
+++ MarsQA-1/SpecflowPages/Pages/SignIn.cs
@@ -24,5 +24,22 @@ namespace MarsQA_1.Pages
             Driver.driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']")).Click();
 
         }
+        public void Login()
+        {
+            Driver.NavigateUrl();
+
+            //Enter Url
+            Driver.driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']")).Click();
+
+            //Enter Username
+            Driver.driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]")).SendKeys("");
+
+            //Enter password
+            Driver.driver.FindElement(By.XPath("//INPUT[@type='password']")).SendKeys("");
+
+            //Click on Login Button
+            Driver.driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']")).Click();
+
+        }
     }
 }
